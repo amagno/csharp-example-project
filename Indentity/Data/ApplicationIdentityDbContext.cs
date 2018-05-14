e3022e9f51bd36a9edf82b9144e3648fd5fc4dfd
alexandre <amagnomonteiro@gmail.com>
2018-05-14 08:33:03 -0300

    identity

---
+++ Indentity/Data/ApplicationIdentityDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Indentity.Models;
+using System.IO;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Configuration;
+
+namespace Indentity.Data
+{
+    public class ApplicationIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationIdentityDbContext(DbContextOptions options) : base(options)
+        {
+
+        
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(Directory.GetCurrentDirectory()).FullName)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
---
+++ Indentity/Models/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+
+namespace Indentity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Email { get; }
+        public string Username { get; }
+       
+        
+        public IList<Claim> GetClaimsFromUser(ApplicationUser user)
+        {
+            return new[]
+            {
+                new Claim("id", new Guid().ToString()), 
+                new Claim("username", user.Username), 
+                new Claim("email", user.Email)
+            };
+        }
+    }
+}
